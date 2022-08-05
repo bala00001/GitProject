e7180984bde71acf9327b72d0eeb25c30d5697b9
bala00001 <bala90206@gmail.com>
2022-08-05 21:27:39 +0530

    mytask

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,74 @@
+package org.emp;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+			
+//		for (int i = 1; i <5; i++) {
+//			System.out.println(i);
+//		}	
+		
+////do-while
+//		
+//		int a=1;
+//		do {
+//			System.out.println(a);
+//			a++;	
+//		}while(a>=3);
+		
+		String tiffen="parota";
+		
+		switch(tiffen) {
+		case "dosa":
+			System.out.println("Dosa Rate 40");
+			break;
+		case "vada":
+			System.out.println("vada 10");
+			break;
+		case "idly":
+			System.out.println("idly 5");
+			break;
+		default:
+			System.out.println("not available");
+		}
+	}
+}
+	
+	
+
+		
+		
+		
+		
+		
+
+
+	
+		
+	
+
+	
+
+        
+		
+     
+
+         
+   
+         
+        
+    
+        		  
+     
+
+       
+	
+	
+
+	
+	    
+   
+      
+	
+
+	 
